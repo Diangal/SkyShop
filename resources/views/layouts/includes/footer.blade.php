393bd65394bddecf57f29c6382ee2a640ec1da6b
Diangal <koutaktmouhameth@gmail.com>
2025-01-15 12:39:21 +0000

    retire les sections about, shop, paniier et compte

--- resources/views/layouts/includes/footer.blade.php
+++ resources/views/layouts/includes/footer.blade.php
@@ -1,5 +1,5 @@
 <footer class="py-5 bg-dark">
-            <div class="container"><p class="m-0 text-center text-white">Copyright &copy; Your Website 2023</p></div>
+            <div class="container"><p class="m-0 text-center text-white">Copyright &copy; <a href="https://accademia.terangacode.com">Teranga Code Accademia</a> </p></div>
         </footer>
         <!-- Bootstrap core JS-->
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
